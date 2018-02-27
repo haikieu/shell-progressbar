fd314b29edadab962b06b09c2c1d9864a8e04eaf
Hai Kieu <haikieu2907@gmail.com>
2018-02-26 16:40:43 -0800

    Update progressbar.sh

--- progressbar.sh
+++ progressbar.sh
@@ -91,18 +91,19 @@ function progress()
 
 echo "The task is in progress, please wait a few seconds"
 
+#Do some tasks
 progress 10 Initialize
-
+#Do some tasks
 progress 20 "Phase 1      "
-
+#Do some tasks
 progress 40 "Phase 2      "
-
+#Do some tasks
 progress 60 "Processing..."
-
+#Do some tasks
 progress 80 "Processing..."
-
+#Do some tasks
 progress 90 "Processing..."
-
+#Do some tasks
 progress 100 "Done        "
 
 echo
